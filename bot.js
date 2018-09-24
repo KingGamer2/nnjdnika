4c0730e62404ec90ddc193350050126666d06b7c
KingGamer2 <40805903+KingGamer2@users.noreply.github.com>
2018-09-24 23:01:02 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -114,35 +114,7 @@ $(prefix)say - يقول البوت التقوله ويحذف رسالتك
 }
 });  
 
-let bane = JSON.parse(fs.readFileSync("./j7.json", "utf8"));
-let banse = new Set();
-client.on('guildBanAdd', function(guild) {
-  guild.fetchAuditLogs().then(logs => {
-    const ser = logs.entries.first().executor;
-    if(!bane[ser.id+guild.id]) bane[ser.id+guild.id] = {
-      bans: 2
-    }
-    let boner = bane[ser.id+guild.id]
-banse.add(ser.id)
-boner.bans = Math.floor(boner.bans+1)
 
-
-setTimeout(() => {
-  boner.bans = 2
-  banse.delete(ser.id)
-},8000)
-
-if(boner.bans > 2) {
-  let roles = guild.members.get(ser.id).roles.array()
-guild.members.get(ser.id).removeRoles(roles)
-}
-
-    })
-    fs.writeFile('./j7.json', JSON.stringify(bane), (err) => {
-if (err) console.error(err);
-})
-
-})
 
 client.on('message', message => {
     if(message.channel.type === 'dm') {
